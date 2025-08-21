8632bb9e50b6d2dd3f18abe704d55a547f6828a1
Arthur Bernard Ferreira Antunes <abernardantunes@gmail.com>
2025-08-20 21:24:51 -0300

    Add files via upload

---
+++ card.js
@@ -0,0 +1,6 @@
+const clearcart = document.getElementsByClassName("clear-cart");
+// Add an event listener to the button
+clearcart.addEventListener('click', function(event) {
+    // When the button is clicked, show an alert with "Olá, mundo"
+    console.log('Olá, mundo');
+});
